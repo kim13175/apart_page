32c0f35128d7ae878ce1f5d13d8bbb709aec8db3
beomjo <dongjun1315@naver.com>
2025-12-12 15:27:45 +0900

    feat(constant): 홍보요소 추가

---
+++ src/Constant/center.js
@@ -0,0 +1,17 @@
+export const CENTER = [
+    {
+        title: "단지 내 즐거움을 더욱 높여주는, 품격있는 커뮤니티 시설",
+        subscribtion: "단지 내 스포츠 시설로 인해 취미와 건강을 모두 누릴 수 있는 최적의 상품",
+        image: "/Image/sports_center.png",
+    },
+    {
+        title: "단지 내 즐거움을 더욱 높여주는, 품격있는 커뮤니티 시설",
+        subscribtion: "단지 내 문화 시설로 인해 아이들과 함께 할 수 있는 북카페와 자녀들의 학습을 위한 프라이빗 독서실 등으로 시설 구성",
+        image: "/Image/culture_center.png",
+    },
+    {
+        title: "내 손안에펼쳐지는 편리함이 적용된 롯데 스마트홈 어플리케이션과  일상의 가치를 높이는 기술력을  담은 특별한 라이프 시스템 적용",
+        subscribtion: "스마트폰 어플리케이션을 설치하여 언제 어디서나 집의 상태를 확인하고 원격으로 제어 가능",
+        image: "/Image/smart_home_care.png",
+    },
+]
--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,3 +1,28 @@
 export const APARTMENT_TITLE = "김해 구산 롯데캐슬 시그니처";
-
 export const TELL_NUMBER = "1844-1646"
+export const OPTION = [
+    {
+        src: "/Image/apart_layout.png",
+        alt: "동호 배치도"
+    },
+    {
+        src: "/Image/84A_type.png",
+        alt: "84A_type",     
+    },
+    {
+        src: "/Image/84B_type.png",
+        alt: "84B_type",     
+    },
+    {
+        src: "/Image/84C_type.png",
+        alt: "84C_type",     
+    },
+    {
+        src: "/Image/84D_type.png",
+        alt: "84D_type",     
+    },
+    {
+        src: "/Image/84E_type.png",
+        alt: "84E_type",     
+    },
+]
