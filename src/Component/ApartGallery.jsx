8f168173ae9e091756631a6f8f995cc37484920f
beomjo <dongjun1315@naver.com>
2025-12-18 13:37:14 +0900

    feat(component/apartgallery): 소개 이미지 컴포넌트 추가

---
+++ src/Component/ApartGallery.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { APART_IMAGE } from "../Constant/constant";
+
+const ApartGallery = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    return (
+        <div className="w-full mx-auto">
+            <div className="relative w-full overflow-hidden shadow-lg bg-gray-100">
+                <img
+                    src={APART_IMAGE[currentIndex].src}
+                    alt={APART_IMAGE[currentIndex].alt}
+                    className="w-full h-[700px] object-cover transition-opacity duration-500 ease-in-out"
+                />
+                <div className="absolute bottom-0 left-0 right-0 bg-linear-to-t from-black/60 p-6">
+                    <p className="text-white text-sm font-bold">{APART_IMAGE[currentIndex].alt}</p>
+                </div>
+            </div>
+            <div className="flex w-full items-center justify-center font-bold gap-3 mt-2">
+                {APART_IMAGE.map((image, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setCurrentIndex(index)}
+                        className={`px-6 py-2 transition-all duration-300 hover-effect
+                        ${currentIndex === index 
+                            ? 'text-amber-600 border-amber-600' 
+                            : 'text-gray-600 hover:text-amber-500'
+                        }`}
+                    >
+                        {image.alt.replace("구산 롯데 캐슬 ", "")}
+                    </button>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ApartGallery
