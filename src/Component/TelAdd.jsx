2be9757c9a5987e5bf842c3498441cf92de727be
beomjo <dongjun1315@naver.com>
2025-12-18 17:15:46 +0900

    fix(teladd): 테두리 추가

--- src/Component/TelAdd.jsx
+++ src/Component/TelAdd.jsx
@@ -1,6 +1,6 @@
 const TelAdd = () => {
     return (
-        <div className="text-2xl sm:text-3xl md:text-4xl flex flex-col justify-center items-center font-bold text-amber-600 gap-2 animate-blink p-8">
+        <div className="text-2xl sm:text-3xl md:text-4xl flex flex-col justify-center items-center font-bold text-amber-600 gap-2 animate-blink p-8 border-2">
             <h3>상담 문의</h3>
             <h3>1844-1646</h3>
         </div>
