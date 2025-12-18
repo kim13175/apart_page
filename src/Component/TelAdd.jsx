6f63cf879956157995e5b906f4f3cc501d684d9e
beomjo <dongjun1315@naver.com>
2025-12-18 17:14:20 +0900

    fix(component/teladd): 안내 문구 글씨 크기 변경

--- src/Component/TelAdd.jsx
+++ src/Component/TelAdd.jsx
@@ -1,6 +1,6 @@
 const TelAdd = () => {
     return (
-        <div className="flex flex-col justify-center items-center text-2xl font-bold text-amber-600 gap-2 animate-blink p-4">
+        <div className="text-2xl sm:text-3xl md:text-4xl flex flex-col justify-center items-center font-bold text-amber-600 gap-2 animate-blink p-8">
             <h3>상담 문의</h3>
             <h3>1844-1646</h3>
         </div>
