23561a71ec91ee786897d45bf56ac9559597d3b7
beomjo <dongjun1315@naver.com>
2025-12-18 17:07:51 +0900

    fix(footer): 하단 바 글자 크기 수정

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -10,12 +10,11 @@ const Footer = () => {
     return (
         <div className="flex flex-col px-10 py-6 gap-4 bg-gray-700 text-white justify-center items-center">
             <h3 className="font-bold">{APARTMENT_TITLE}</h3>
-            <p className="text-gray-400 text-center whitespace-nowrap">{APARTMENT_TITLE} | 경상남도 김해시 구산동 583</p>
+            <p className="text-xs sm:text-sm md:text-md text-gray-400 text-center whitespace-nowrap">{APARTMENT_TITLE} | 경상남도 김해시 구산동 583</p>
             <div className="text-center">
                 <p className="whitespace-pre-line text-xs text-gray-400">{footerSection}</p>
             </div>
             <div className="text-center text-sm font-bold">
-                <p className="mb-2">시공사: (주)롯데건설</p>
                 <p>{TELL_NUMBER}</p>
             </div>
         </div>
